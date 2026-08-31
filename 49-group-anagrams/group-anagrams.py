66e0d2c203eafc881dbd3254be4e0c525441b663
Hardik Rawat <hardikrawat79@gmail.com>
2026-09-01 02:36:29 +0530

    Time: 27 ms (28.10%) | Memory: 18.4 MB (5.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -6,13 +6,9 @@ class Solution(object):
         """
         "Hardik"
         res=defaultdict(list)
-
         for s in strs:
             count=[0]*26
-
             for c in s:
                 count[ord(c) - ord('a')] += 1
-
             res[tuple(count)].append(s)
-
         return list(res.values())
