549a3ea9970bdf80b43c62038d4b13483943beb1
Hardik Rawat <hardikrawat79@gmail.com>
2026-09-04 03:56:58 +0530

    Time: 870 ms (61.29%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
 SELECT
     p.product_id,
-    IFNULL(ROUND(SUM(p.price * us.units) / SUM(us.units),2),0) AS average_price
+    IFNULL(ROUND(SUM(p.price * us.units) / SUM(us.units),2),0) 
+    AS average_price
 FROM
     Prices AS p
 LEFT JOIN 
