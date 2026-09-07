1afbf1717d63fc58f45d5660d4e3ef41c86a255a
Hardik Rawat <hardikrawat79@gmail.com>
2026-09-07 05:33:52 +0530

    Time: 480 ms (63.77%) | Memory: 0B (100.00%) - LeetSync

--- 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -3,7 +3,5 @@ SELECT
     sell_date,
     COUNT(DISTINCT product) AS num_sold,
     GROUP_CONCAT(DISTINCT product ORDER BY product) AS products
-FROM
-    Activities
-GROUP BY
-    sell_date
+FROM Activities
+GROUP BY sell_date
