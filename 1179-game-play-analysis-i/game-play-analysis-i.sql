234b055af6f93a3d97ab4f7ccf367b69f12c05a3
Hardik Rawat <hardikrawat79@gmail.com>
2026-09-08 12:27:10 +0530

    Time: 674 ms (14.44%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
 SELECT
-    DISTINCT player_id,
-    FIRST_VALUE(event_date) OVER(PARTITION BY player_id ORDER BY event_date) 
+    DISTINCT player_id, FIRST_VALUE(event_date) OVER(PARTITION BY player_id ORDER BY event_date) 
 AS 
     first_login
 FROM
