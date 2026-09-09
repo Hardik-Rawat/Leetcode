87a5e64c16df81a61b43c203d362731c1c21b832
Hardik Rawat <hardikrawat79@gmail.com>
2026-09-09 23:59:50 +0530

    Time: 646 ms (39.30%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,12 +1,10 @@
 # Write your MySQL query statement below
 SELECT
     employee_id,
-    department_id
-FROM Employee
-WHERE primary_flag = 'Y'
-   OR employee_id IN (
-       SELECT employee_id
-       FROM Employee
-       GROUP BY employee_id
-       HAVING COUNT(*) = 1
-   );
+    CASE WHEN COUNT(department_id ) = 1 THEN department_id 
+    WHEN COUNT(department_id )>1 THEN SUM((primary_flag='Y')*department_id ) ELSE department_id  END
+    AS department_id
+FROM
+    Employee
+GROUP BY
+    employee_id
