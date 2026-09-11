d9457a5caee60aa56a4de1e520960803307e6764
Hardik Rawat <hardikrawat79@gmail.com>
2026-09-11 23:13:12 +0530

    Time: 1085 ms (34.90%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -2,7 +2,7 @@
 WITH cte AS
 (SELECT
     person_name,
-    SUM(weight) OVER(ORDER BY turn) AS sum_weight
+    SUM(weight) OVER(ORDER BY turn ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS sum_weight
 FROM
     Queue)
 SELECT
